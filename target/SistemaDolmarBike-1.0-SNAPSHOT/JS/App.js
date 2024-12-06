80ec2ce46681c7b618ef219008968988b8f90716
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2024-12-05 19:41:41 -0500

    Eliminar carpeta target y añadirla al .gitignore

--- target/SistemaDolmarBike-1.0-SNAPSHOT/JS/App.js
+++
@@ -1,7 +0,0 @@
-function confirmarEliminacion(idCliente) {
-    var confirmacion = confirm("¿Estás seguro de que deseas eliminar este cliente?");
-
-    if (confirmacion) {
-        document.getElementById("formEliminar_" + idCliente).submit();
-    }
-}
